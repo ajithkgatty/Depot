450623bc1afd76a1804d403bcb2bb6549b2b3cd5
ajithkgatty <ajithkgatty@gmail.com>
2016-02-10 06:12:23 -0800

    minor modification on the look and feel

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -28,8 +28,8 @@ class ProductsController < ApplicationController
 
     respond_to do |format|
       if @product.save
-        format.html { redirect_to @product, notice: 'Product was successfully created.' }
-        format.json { render :show, status: :created, location: @product }
+        format.html { redirect_to products_url, notice: 'Product was successfully created.' }
+       # format.json { render :index, status: :created, location: @product }
       else
         format.html { render :new }
         format.json { render json: @product.errors, status: :unprocessable_entity }
@@ -42,6 +42,7 @@ class ProductsController < ApplicationController
   def update
     respond_to do |format|
       if @product.update(product_params)
+
         format.html { redirect_to @product, notice: 'Product was successfully updated.' }
         format.json { render :show, status: :ok, location: @product }
       else
